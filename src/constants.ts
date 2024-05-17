dbf55525849ac93c120ed1cd7ed0e532a2d8a863
Juan Peñalba <juantandil123@gmail.com>
2024-05-17 13:28:08 -0300

    finish

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,9 @@
 export const COLORS = { primary: "#42736f" };
+
+export const CONTACT = {
+  instagram: "instagram.com",
+  gmail: "mamparasesmeralda@gmail.com",
+  facebook: "facebook.com",
+  phone: "+54 2494377662",
+  whatsapp: "https://wa.link/8zqvfp",
+};
